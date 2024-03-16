4d86ccf1e0281d7b0f9cdab53a8009e2ba1f029c
aifp1 <fuentesandres31@gmail.com>
2024-03-16 15:42:03 -0300

    Se agregan los componentes al principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { PrimengModule } from '../modules/primeng/primeng.module'
+
+import { NavBarComponent } from '../components/nav-bar/nav-bar.component'
+import { GalleryComponent } from '../components/gallery/gallery.component'
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    //Components
+    NavBarComponent,
+    GalleryComponent,
+    //Modules
+    PrimengModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
